b9380d70cd5e34ffa9458026f8a9fd1301ed6a4f
RomanSamohwal <rsamohwal@gmail.com>
2020-12-13 00:06:11 +0300

    first commit. Second commit

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MonthContainer } from '../components/month/MonthContainer';
+import {Test} from '../components/test/test';
+
+function App() {
+    return (
+        <div>
+            <MonthContainer/>
+           {/* <Test/>*/}
+        </div>
+    );
+}
+
+export default App;
